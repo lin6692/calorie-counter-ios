bf53d27b6f9fe6bca454850f11d4ac56d12ccdf7
Lin Liu <lin_6066@yahoo.com>
2022-08-05 14:24:11 -0400

    initial project

---
+++ calorie-counter-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calorie-counter-ios
+//
+//  Created by Lin Liu on 8/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ calorie-counter-ios/calorie_counter_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  calorie_counter_iosApp.swift
+//  calorie-counter-ios
+//
+//  Created by Lin Liu on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct calorie_counter_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
