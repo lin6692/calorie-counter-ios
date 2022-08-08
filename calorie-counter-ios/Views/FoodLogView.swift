51fed451441dbba00656fa46342f599b6e9bb0af
Lin Liu <lin_6066@yahoo.com>
2022-08-08 01:01:05 -0400

    start building view for food log

--- calorie-counter-ios/Views/FoodLogView.swift
+++ calorie-counter-ios/Views/FoodLogView.swift
@@ -6,10 +6,32 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct FoodLogView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \FoodEntity.date, ascending: false)],
+        animation: .default)
+        private var foods: FetchedResults<FoodEntity>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            List {
+                ForEach(foods) { food in
+                    Text(food.name ?? "")
+                }
+                .onDelete(perform: deleteFood)
+            }
+        }
+    }
+    
+    private func deleteFood(offsets:IndexSet) {
+        withAnimation {
+            offsets.map{ foods[$0] }.forEach(viewContext.delete)
+            saveFood(context:viewContext)
+        }
     }
 }
 
