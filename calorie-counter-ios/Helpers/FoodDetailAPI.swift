83b9582b3aaba2b6d7906d79b8b301888b68da80
Lin Liu <lin_6066@yahoo.com>
2022-08-08 00:59:16 -0400

    make api file to retrive food calorie data

--- calorie-counter-ios/Helpers/FoodDetailAPI.swift
+++ calorie-counter-ios/Helpers/FoodDetailAPI.swift
@@ -5,4 +5,50 @@
 //  Created by Lin Liu on 8/7/22.
 //
 
-import Foundation
+import SwiftUI
+
+struct JSONParameters:Encodable {
+    let search_term: String
+}
+
+struct FoodDetail:Codable, Identifiable {
+    let id = UUID()
+    var name: String
+    var ounce: [Int]
+    var serving: [Int]
+    var slice: [Int]
+    var whole: [Int]
+}
+
+func convertIntoJSONString(arrayObject: [Any]) -> String? {
+
+        do {
+            let jsonData: Data = try JSONSerialization.data(withJSONObject: arrayObject, options: [])
+            if  let jsonString = NSString(data: jsonData, encoding: String.Encoding.utf8.rawValue) {
+                return jsonString as String
+            }
+            
+        } catch let error as NSError {
+            print("Array convertIntoJSON - \(error.description)")
+        }
+        return nil
+    }
+
+class Api {
+    func getInfo(search_term:String, completion: @escaping (FoodDetail) -> () ) {
+        var components = URLComponents(string: "http://127.0.0.1:5000/calories")!
+        components.queryItems = [
+            URLQueryItem(name:"search_term", value: search_term)
+        ]
+        
+        var request = URLRequest(url: components.url!)
+        URLSession.shared.dataTask(with: request) { data, _, _ in
+           
+            let foodInfo = try! JSONDecoder().decode(FoodDetail.self, from:data!)
+            DispatchQueue.main.async {
+                completion(foodInfo)
+            }
+        }
+        .resume()
+    }
+}
