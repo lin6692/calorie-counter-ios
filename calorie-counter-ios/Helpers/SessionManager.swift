46e75fc10a1520811d1d0cb13fac9b4d6ebec145
Lin Liu <lin_6066@yahoo.com>
2022-08-10 17:18:26 -0400

    add session manager to manage current user session

--- calorie-counter-ios/Helpers/SessionManager.swift
+++ calorie-counter-ios/Helpers/SessionManager.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import SwiftUI
+import Firebase
+import FirebaseFirestore
+
+class SessionManager: NSObject, ObservableObject {
+    
+    @Published var loggedUser: User?
+    static let shared = SessionManager()
+    private let auth = Auth.auth()
+    
+    var handle : AuthStateDidChangeListenerHandle?
+    
+    override private init() {
+        loggedUser = auth.currentUser
+        super.init()
+        handle = auth.addStateDidChangeListener(authStateChanged)
+    }
+    
+    private func authStateChanged(with auth: Auth, user: User?) {
+        guard user != self.loggedUser else { return }
+        self.loggedUser = user
+    }
+        
+    func logout() {
+        do {
+            try Auth.auth().signOut()
+        } catch(let error) {
+            debugPrint(error.localizedDescription)
+        }
+    }
+    
+    func unbind() {
+        if let handle = handle {
+            Auth.auth().removeStateDidChangeListener(handle)
+        }
+    }
+    
+    deinit {
+        unbind()
+    }
+    
+}
--- calorie-counter-ios/calorie_counter_iosApp.swift
+++ calorie-counter-ios/calorie_counter_iosApp.swift
@@ -6,15 +6,32 @@
 //
 
 import SwiftUI
+import Firebase
+import GoogleSignIn
 
 @main
 struct calorie_counter_iosApp: App {
-
+    // connecting AppDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
-            HomeView()
-//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            HomeView().environmentObject(SessionManager.shared)
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate{
+    func application(_ application:UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool{
+        
+        // Initializing FireBase
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ application: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any])
+      -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
     }
 }
