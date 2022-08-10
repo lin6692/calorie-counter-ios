29365b2aeb158ac01ea3dbc698108d6912b194b2
Lin Liu <lin_6066@yahoo.com>
2022-08-10 17:17:45 -0400

    update datamodel to filter local calorie intake info for user

--- calorie-counter-ios/DataModel/FoodViewModel.swift
+++ calorie-counter-ios/DataModel/FoodViewModel.swift
@@ -7,12 +7,14 @@
 
 import CoreData
 import SwiftUI
+import Firebase
 
 class FoodViewModel: ObservableObject {
     
     let container: NSPersistentContainer
     @Published var foods:[FoodEntity] = []
     @Published var calorieToday:Int  = 0
+    
     
     init() {
         container = NSPersistentContainer(name:"calorie-counter-ios")
@@ -28,20 +30,27 @@ class FoodViewModel: ObservableObject {
         let fetchRequest: NSFetchRequest<FoodEntity> = FoodEntity.fetchRequest()
         let sort = NSSortDescriptor(keyPath: \FoodEntity.date, ascending: false)
         fetchRequest.sortDescriptors = [sort]
+        
+//        let predicate = NSPredicate(format: "userUID == %@", userUID ?? "")
+//        fetchRequest.predicate = predicate
+        
         do {
             foods = try container.viewContext.fetch(fetchRequest)
-            calorieToday = totalCaloriesToday()
            } catch {
                print("Failed to fetch movies: \(error)")
            }
     }
     
-    func addFood(name:String, calorie:Int) {
+
+    
+    func addFood(name:String, calorie:Int, user:Person) {
         let newFood = FoodEntity(context:container.viewContext)
         newFood.id = UUID()
         newFood.date = Date()
         newFood.name = name
         newFood.calorie = Int64(exactly: calorie)!
+        newFood.userEmail = user.email
+        print("added!!! \(newFood.userEmail)")
         saveFood()
     }
     
@@ -62,11 +71,10 @@ class FoodViewModel: ObservableObject {
         }
     }
     
-    func totalCaloriesToday() -> Int {
+    func totalCaloriesToday(user:Person) -> Int {
         var caloriesToday:Int = 0
-//        getAllFoods()
         for food in foods {
-            if Calendar.current.isDateInToday(food.date!) {
+            if Calendar.current.isDateInToday(food.date!) && food.userEmail == user.email {
                 caloriesToday += Int(food.calorie)
             }
         }
